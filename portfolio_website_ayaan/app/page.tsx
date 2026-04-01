422a80a4898a884b8f3ab71073431393a461b89b
Ayaan Karkera <ayaankarkera8@gmail.com>
2026-04-01 21:00:24 +0530

    Pfp added

--- portfolio_website_ayaan/app/page.tsx
+++ portfolio_website_ayaan/app/page.tsx
@@ -2,20 +2,23 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="flex bg-[#E9E3DF] min-h-[200vh] pt-20 font-[Basic]">
-      <div className="realtive w-[305px] h-[305px] flex items-center justify-center bg-gradient-to-tr from-orange-500 via-pink-500 to-yellow-400 rounded-full p-[5px]">
-        <div>
-          <div className="relative w-[300px] h-[300px] translate-x-[430px] translate-y-[200px] ">
-            <Image
-              src='/cropped_circle_image.png'    
-              fill
-              className="object-contain"
-              alt="Image Of Ayaan"
-              priority
-            />
+    <div className="flex bg-[#E9E3DF] min-h-[100vh] pt-20 flex-col items-center">
+
+  
+      <div className="relative flex items-center justify-center w-[500px] h-[500px] bg-[radial-gradient(circle,_#FF7A30_0%,_#FF7A30_20%,_#E9E3DF_45%)] mt-[50px] ml-[-600px]">
+  
+        <div className="relative w-[200px] h-[200px] rounded-full overflow-hidden shadow-2xl">
+          <Image 
+            src='/cropped_circle_image.png'
+            alt="Image Of Ayaan"
+            fill
+            className="object-cover z-20" // Added z-20 here
+            priority
+          />
         </div>
-        </div>
+
       </div>
-    </div>
+
+</div>
   );
 }
